512b9186f16be5f8b3388976e3fdc41680acc72c
blanebussey <blanebussey@gmail.com>
2021-10-24 20:05:57 -0500

    initial jquery,updating of html for textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,30 +3,24 @@ var now = moment().format("dddd, MMMM Do, YYYY, h:mm:ss A");
      // Saturday, June 9th, 2007, 5:46:21 PM
 $('#currentDay').append(now);
 
-var tasks = {};
-
-var createTask = function(taskText, taskTime, taskList) {
-var taskLi = $("<li>").addClass("list-group-item");
-var taskSpan = $("<span>").text(taskTime);
-var taskP = $("<p>").addClass("m-1").text(taskText)
-//appen span and p element to parent li 
-taskLi.append(taskSpan, taskP );
-//append to ul list on the page
-$("list-" + taskList).append(taskLi);
-};
+$(".saveBtn").on("click", function(){
+     var time = $(this).parent().attr('id')
+     var task = $(this).siblings('.description').val()
+     localStorage.setItem(time,task)
+})
 
 
 var saveTasks = function() {
      localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-
-//modal was triggered
-$("#task-form-modal").on("show.bs.modal"), function() {
-     //clear values
-     $("#modalTaskDescription", "#modalDueDate").val("");
-};
-
-$("task-form-modal .btn-primary").click(function(){
-
-});
+$('#9am .description').val(localStorage.getItem('9am'));
+$('#10am .description').val(localStorage.getItem('10am'));
+$('#11am .description').val(localStorage.getItem('11am'));
+$('#12pm .description').val(localStorage.getItem('12pm'));
+$('#1pm .description').val(localStorage.getItem('1pm'));
+$('#2pm .description').val(localStorage.getItem('2pm'));
+$('#3pm .description').val(localStorage.getItem('3pm'));
+$('#4pm .description').val(localStorage.getItem('4pm'));
+$('#5pm .description').val(localStorage.getItem('5pm'));
+$('#6pm .description').val(localStorage.getItem('6pm'));
